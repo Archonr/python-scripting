a7b101bf8c05d325b8364241a431d258f5b6c0bb
Vlad Korkach <Archonr94@gmail.com>
2019-03-21 15:23:31 +0200

    changes

--- backup-notification-telegram/backup.py
+++ backup-notification-telegram/backup.py
@@ -4,7 +4,7 @@ import requests
 retcodeSite=subprocess.call(r'"C:\Program Files (x86)\WinSCP\winscp.com" /ini=nul /console /command "option batch on" "option confirm off" "open sftp://root@host -hostkey=""ssh-rsa 2048 TESTaJzqFfGNiNPX9vOwVz7Di1mgJzxAeW/Sx37gsV2="" -privatekey=C:\Script\backup.ppk" "synchronize -criteria=time -transfer=binary remote D:\path\ /path/to/remote/" "exit" ', shell=True)
 
 if retcodeSite == 0:
-	url = "https://api.telegram.org/bot404295605:AAHZL-ZwKxrkHnz1HFwwhp4O2vn-8yiFqb8/"
+	url = "https://api.telegram.org/botid:AAHZL-ZwKxrkHnz1HFwwas4O2vn-8yiFqb8/"
 	def get_updates_json(request):  
 		response = requests.get(request + 'getUpdates')
         	return response.json()
@@ -23,7 +23,7 @@ if retcodeSite == 0:
 	send_mess(201837960, 'OK: S-grain Site + Mail')
 
 else:
-        url = "https://api.telegram.org/bot404295605:AAHZL-ZwKxrkHnz1HFwwhp4O2vn-8yiFqb8/"
+        url = "https://api.telegram.org/botid:AAHZL-ZwKxrkHnz2dswwhp4O2vn-8yiFqb8/"
         def send_mess(chat, text):  
                 params = {'chat_id': chat, 'text': text}
                 response = requests.post(url + 'sendMessage', data=params)
