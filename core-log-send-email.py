dc56e6ddaedc2678721ea78afa26b78a64ed2bb9
Vlad Korkach <Archonr94@gmail.com>
2019-07-09 15:59:38 +0300

    Update core-log-send-email.py

--- core-log-send-email.py
+++ core-log-send-email.py
@@ -3,25 +3,34 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
-import os.path
+import os.path, time
 import os
 import tarfile
 
 for exists in os.listdir('/tmp'):
-    if exists.startswith('core-'):
+    if exists.startswith('core-gameserver'):
     # Store configuration file values
+        time = time.strftime('%Y-%m-%d', time.gmtime(os.path.getmtime('/data/gameserver_logs')))
         with tarfile.open('core-dump.tar.gz', mode='w:gz') as tf:
-            tf.add('/data/logs')
+            logdir = ['/data/gameserver_logs', '/data/gameserver_logs/install', '/data/gameserver_logs/msg', '/data/gameserver_logs/site', '/data/gameserver_logs/wrn', '/data/gameserver_logs/db', '/data/gameserver_logs/debug', '/data/gameserver_logs/err']
+            for lgdir in logdir:
+                pathlog = lgdir
+                #print(pathlog)
+            #print (lgdir)
+                for game in os.listdir(pathlog):
+                    if game.startswith(time):
+                        #print(pathlog + game)
+                        tf.add(pathlog + '/' + game)
             for core in os.listdir('/tmp'):
-                if core.startswith('core-'):
+                if core.startswith('core-gameserver'):
                     tf.add('/tmp/' + core)
         tf.close()
 
-        email = 'ooo@ooo'
-        password = 'xxxxxxxx'
-        send_to_emails = ['yyy@yyy', 'xxx@xxx'] # List of email addresses
-        subject = 'This is the subject'
-        message = 'This is my message'
+        email = 'phptest@pls.life'
+        password = 'ooLLueeE7rcv'
+        send_to_emails = ['v.korkach@pls.life', 'y.kosinskiy@pls.life', 'd.mosnenko@pls.life', 's.yarmak@pls.life', 's.lakhovets@pls.life'] # List of email addresses
+        subject = 'This is core dump from gameserver'
+        message = 'Archive in attachment'
         file_location = '/data/core-dump.tar.gz'
 
 # Create the attachment file (only do it once)
@@ -56,7 +65,7 @@ for exists in os.listdir('/tmp'):
         server.quit()
         os.remove("core-dump.tar.gz")
         for corerm in os.listdir('/tmp'):
-            if corerm.startswith('core-'):
+            if corerm.startswith('core-gameserver'):
                 os.remove('/tmp/' + corerm)
 else:
     print("Not exist file")
