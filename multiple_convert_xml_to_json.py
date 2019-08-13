83a8a46ecc41b559fc733727ed2b2830d923335e
Vlad Korkach <Archonr94@gmail.com>
2019-08-13 17:35:22 +0300

    Update multiple_convert_xml_to_json.py

--- multiple_convert_xml_to_json.py
+++ multiple_convert_xml_to_json.py
@@ -2,12 +2,15 @@ import xmltodict, json
 import os
 import re
 
-for exists in os.listdir('/directory_to_xml_file/'):
+dir = "/directory_where_xml_files/"
+
+for exists in os.listdir(dir):
     exists = re.findall("(.*xml$)", exists)
     for xml in exists:
-        with open("/directory_to_xml_file/"+xml, 'rw') as fin:
+        with open(dir+xml, 'rw') as fin:
             xml = xml.split('.')[0]
-            obj = open('/directory_to_xml_file/{}.json'.format(xml), 'wb')
+            obj = open(dir+'{}.json'.format(xml), 'wb')
             doc = xmltodict.parse(fin.read())
             data = json.dumps(doc, indent=True)
+            #print data
             obj.write(data)
